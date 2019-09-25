7e6bd18fa68f6ff485875b09bfd06a36ef33c653
Payton Musco <pjmusco.2017@mymail.becker.edu>
2019-09-24 22:10:45 -0400

    Added Visuals for player Health
    Added slider and health counter to top left to show that health system is working correctly

--- Cell-Team-6-Game/Assets/Scripts/HealthScript.cs
+++ Cell-Team-6-Game/Assets/Scripts/HealthScript.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class HealthScript : MonoBehaviour
 {
     //Created necessary Variables
+    public Slider Playerhealth;
+    public Text Healthtext;
     public int currentHealth;
     private bool isplayer = false;
     private int Damage;
 
     void Start()
-    {   
+    {
+        Playerhealth = FindObjectOfType<Slider>();
+        Healthtext = FindObjectOfType<Text>();
         //Tells the script wether to treat the gameobject as a player or an enemy, and set the health accordingly
         if (gameObject.CompareTag("Player"))
         {
@@ -19,6 +24,7 @@ public class HealthScript : MonoBehaviour
         }
         else
             currentHealth = 100;
+        Playerhealth.maxValue = currentHealth;
     }
 
     //Can be called to restore 1 heart to the player
@@ -30,13 +36,19 @@ public class HealthScript : MonoBehaviour
     //Is called to remove one heart from the player
     private void Player_Take_Damage()
     {
-        currentHealth--;
+        if (currentHealth > 0)
+            currentHealth--;
+        else
+            Die();
     }
 
     //Is passed a damage value from the collision function, and subtracts the damage from the current health of the enemy
     private void Enemy_Take_Damage(int damageValue)
     {
-        currentHealth -= damageValue;
+        if (currentHealth > 0)
+            currentHealth -= damageValue;
+        else
+            Die();
     }
 
     //On collision
@@ -44,16 +56,28 @@ public class HealthScript : MonoBehaviour
     {
         //If this is attatched to the player, and the object colliding is an enemy or an enemy bullet, subract a heart, and destroy the object that hit it
         if(isplayer && (collision.gameObject.CompareTag("EnemyBullet")|| collision.gameObject.CompareTag("Enemy")))
-        {
-            Destroy(collision.gameObject);
+        {   
+            if(collision.gameObject.CompareTag("Enemy"))
+                Destroy(collision.gameObject);
             Player_Take_Damage();
         }
         //If it is not a player, and the collision is a player bullet, pass in the damage value from the bullet and call the enemy damage function
-        else if(!isplayer && collision.gameObject.CompareTag("PLayerBullet"))
+        else if(!isplayer && collision.gameObject.CompareTag("PlayerBullet"))
         {
-            //Damage = collision.Damage();
+            Damage = collision.gameObject.GetComponent<SimpleBullet>().Damage();
             Enemy_Take_Damage(Damage);  
         }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        Playerhealth.value = currentHealth;
+        Healthtext.text = "Health: " + currentHealth;
     }
 
 }
