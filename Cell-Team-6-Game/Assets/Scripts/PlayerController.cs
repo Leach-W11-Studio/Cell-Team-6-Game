0a4f18fa82f79b78a0d6f3d848a14b5766af4bf8
Adam Giunta <amgiunta.2016@mymail.becker.edu>
2019-09-24 22:48:57 -0400

    added walking and hitbox indicator

--- Cell-Team-6-Game/Assets/Scripts/PlayerController.cs
+++ Cell-Team-6-Game/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CircleCollider2D))]
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    [Tooltip("The number to divide the moveSpeed by when walking")]
+    public float walkModifier;
+
+    public bool isWalking;
+    GameObject hitboxHighlight;
+
+    private void Start()
+    {
+        hitboxHighlight = transform.Find("Hitbox Highlight").gameObject;
+    }
 
     void Movement()
     {
+        float targetSpeed = isWalking ? moveSpeed / walkModifier : moveSpeed;
         //Makes object move in an absolute fashion in all 4 directions
-        float horizontal = Input.GetAxis("Horizontal") * moveSpeed/10;
-        float vertical = Input.GetAxis("Vertical") * moveSpeed/10;
+        float horizontal = Input.GetAxis("Horizontal") * targetSpeed/10;
+        float vertical = Input.GetAxis("Vertical") * targetSpeed/10;
         transform.position = new Vector2(transform.position.x + horizontal, transform.position.y + vertical);
     }
 
@@ -24,6 +36,15 @@ public class PlayerController : MonoBehaviour
         float newRot = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
         newRot -= 90.0f;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, newRot));
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Walk")) { isWalking = true; }
+        else { isWalking = false; }
+
+        if (isWalking) { hitboxHighlight.SetActive(true); }
+        else { hitboxHighlight.SetActive(false); }
     }
 
     void FixedUpdate()
