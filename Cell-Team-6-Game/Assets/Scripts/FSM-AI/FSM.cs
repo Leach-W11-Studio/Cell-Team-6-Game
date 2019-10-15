2dd4daeedfbcd3d4ef90f4c3e69a876da20f03d7
Ben Shackman <Bshackman@protonmail.com>
2019-10-15 10:36:27 -0400

    Finished FSM Framework

--- Cell-Team-6-Game/Assets/Scripts/FSM-AI/FSM.cs
+++ Cell-Team-6-Game/Assets/Scripts/FSM-AI/FSM.cs
@@ -11,6 +11,13 @@ public enum FSMStateID
     PlaceholderState,
 }
 
+public enum FSMTransitions
+{
+    none = 0,
+    PlaceholderTrans1,
+    PlaceholderTrans2,
+}
+
 public abstract class FSM : MonoBehaviour
 {
     public Transform playerTransform;
@@ -21,17 +28,21 @@ public abstract class FSM : MonoBehaviour
     protected abstract void Initalize();
     protected abstract void FSMUpdate();
     protected abstract void FSMFixedUpdate();
-    
+
     private FSMState currentState;
     public FSMState CurrentState { get { return currentState; } }
 
     private List<FSMState> FSMStates = new List<FSMState>();
 
+    /// <summary>
+    /// Adds an FSM state to the list of currently avalable states. First added state is treated as inital state as well
+    /// </summary>
+    /// <param name="state">State which is to be added</param>
     public void AddFSMState(FSMState state)
     {
         foreach (var item in FSMStates)
         {
-            if(item.StateID == state.StateID)
+            if (item.StateID == state.StateID)
             {
                 Debug.LogError("Error: You can not add a state that is already present in this FSM");
                 return;
@@ -39,7 +50,7 @@ public abstract class FSM : MonoBehaviour
         }
 
         //First state added is treated as inital starting state for purposes of this FSM
-        if(FSMStates.Count == 0)
+        if (FSMStates.Count == 0)
         {
             FSMStates.Add(state);
             currentState = state;
@@ -50,46 +61,60 @@ public abstract class FSM : MonoBehaviour
         }
     }
 
-    public void RemoveFSMState(FSMState state)
+    /// <summary>
+    /// Removes FSMState from list of currently avalable FSM States
+    /// </summary>
+    /// <param name="stateID">State which is to be removed</param>
+    public void RemoveFSMState(FSMStateID stateID)
     {
         foreach (var item in FSMStates)
         {
-            if(item.StateID == state.StateID)
+            if (item.StateID == stateID)
             {
                 FSMStates.Remove(item);
                 return;
             }
         }
-        Debug.LogError("Error: " + state.StateID.ToString() + " Is not present within FSMStates");
+        Debug.LogError("Error: " + stateID.ToString() + " Is not present within FSMStates");
     }
 
-    public void SetState(FSMState state)
+    /// <summary>
+    /// Sets new state based off input transition
+    /// </summary>
+    /// <param name="trans">Transition to be set to given state</param>
+    public void SetTransition(FSMTransitions trans)
     {
-        foreach (var item in FSMStates)
+        if (trans == FSMTransitions.none) { Debug.LogError("Null transition is not allowed"); return; }
+        FSMStateID newID = currentState.CheckTransition(trans);
+        if (newID == FSMStateID.none) { Debug.LogError("There is no state bound to " + trans.ToString()); return; }
+        foreach (var state in FSMStates)
         {
-            if(item.StateID == state.StateID)
+            if(newID == state.StateID)
             {
-                currentState = item;
+                Debug.Log("State changed to " + newID.ToString());
+                currentState = state;
                 return;
             }
         }
-        Debug.LogError("Error: " + state.StateID.ToString() + " Is not present within FSMStates");
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         Initalize();
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentState.Reason(playerTransform, gameObject);
         FSMUpdate();
     }
 
     void FixedUpdate()
     {
+        currentState.Act(playerTransform, gameObject);
         FSMFixedUpdate();
     }
 }
