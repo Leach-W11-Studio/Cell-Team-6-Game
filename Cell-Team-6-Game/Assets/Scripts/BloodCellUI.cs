1db1a354d7e1a7e44f3a653fac33eaef6bf3f5a8
Adam Giunta <amgiunta.2016@mymail.becker.edu>
2019-10-27 17:25:04 -0400

    UI buttons

--- Cell-Team-6-Game/Assets/Scripts/BloodCellUI.cs
+++ Cell-Team-6-Game/Assets/Scripts/BloodCellUI.cs
@@ -8,20 +8,17 @@ public class BloodCellUI : MonoBehaviour
     public PlayerInventory.BulletType cellType;
 
     Text amountText;
-    Text cellTypeText;
     PlayerInventory playerInventory;
 
     // Start is called before the first frame update
     void Start()
     {
         amountText = transform.Find("Amount").GetComponent<Text>();
-        cellTypeText = transform.Find("Cell Type").GetComponent<Text>();
         playerInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
     }
 
     private void UpdateUI() {
         amountText.text = playerInventory.whiteBloodCells [cellType].ToString();
-        cellTypeText.text = cellType.ToString();
     }
 
     // Update is called once per frame
