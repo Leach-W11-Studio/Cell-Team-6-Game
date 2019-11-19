649979ad1970a5a6ddc1bda6bc5177e5a01385f3
Brandon Pereira <brandon0tor2@gmail.com>
2019-11-19 11:35:45 -0500

    Tried Working on Health

---
+++ Cell-Team-6-Game/Assets/Heart_Sound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart_Sound : MonoBehaviour
+{
+    
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+  
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SoundManager.PlaySound(gameObject.GetHashCode(), "PickUp_Health");
+        }
+    }
+}
