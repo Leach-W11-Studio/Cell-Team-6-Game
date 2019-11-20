ba12e7d1328441ea1aee0f6553fe1de72a15a100
Adam Giunta <amgiunta.2016@mymail.becker.edu>
2019-11-20 13:52:56 -0500

    prefab fix

--- Cell-Team-6-Game/Assets/WaterBullet.cs
+++ Cell-Team-6-Game/Assets/WaterBullet.cs
@@ -9,11 +9,6 @@ public class WaterBullet : SimpleBullet
     public float explosionTime;
     public float explosionAmount = 2;
     public LayerMask layerMask;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("WaterBullet");
-    }
 
     // Update is called once per frame
     void Update()
@@ -21,15 +16,25 @@ public class WaterBullet : SimpleBullet
         
     }
 
-    private IEnumerator ExplodeGroup (List<Collider2D> targetList) {
+    public IEnumerator ExplodeGroup (List<Collider2D> targetList) {
         foreach (Collider2D collider in targetList) {
-            StartCoroutine(Explode(collider));
+            
+            HealthScript hs = collider.gameObject.GetComponent<HealthScript>();
+
+            if (!instantKill) {
+                hs.TakeDamage(Damage());
+            }
+            else {
+                StartCoroutine(Explode(collider));
+            }
+
             yield return new WaitForSeconds(Random.Range(0, explosionTime/targetList.Count));
         }
+        gameObject.SetActive(false);
     }
 
-    private IEnumerator Explode (Collider2D target) {
-        Vector3 targetScale = target.transform.localScale*explosionAmount;
+    public IEnumerator Explode (Collider2D target) {
+        Vector3 targetScale = target.transform.localScale * explosionAmount;
         for (float elapsedTime = 0; elapsedTime < explosionTime; elapsedTime += Time.deltaTime) {
             float percent = elapsedTime/explosionTime;
             target.transform.localScale = Vector3.Lerp(target.transform.localScale, targetScale, percent);
@@ -39,25 +44,20 @@ public class WaterBullet : SimpleBullet
         target.GetComponent<HealthScript>().Die();
     }
 
-    protected void OnTriggerEnter2D (Collision2D collision) {
-        if (collision.gameObject.CompareTag("Enemy")) {
-            HealthScript hs = collision.gameObject.GetComponent<HealthScript>();
-
-            List<Collider2D> objects = new List<Collider2D>(Physics2D.OverlapCircleAll(transform.position, explodeRadius, layerMask));
-            for (int i = 0; i < objects.Count; i++) {
-                if (!objects[i].CompareTag("Enemy")) {
-                    objects.Remove(objects[i]);
-                }
-            }
-
-            if (instantKill) {
-                StartCoroutine(ExplodeGroup(objects));
-            }
-            else {
-                hs.TakeDamage(Damage());
-            }
-
-            gameObject.SetActive(false);
+    protected void OnTriggerEnter2D (Collider2D collision) {
+        if (collision.CompareTag("Player") || collision.CompareTag("Untagged")) {
+            return;
         }
+
+        Debug.Log(collision.tag);
+        List<Collider2D> objects = new List<Collider2D>(Physics2D.OverlapCircleAll(transform.position, explodeRadius, layerMask));
+        for (int i = 0; i < objects.Count; i++) {
+            if (!objects[i].CompareTag("Enemy")) {
+                objects.Remove(objects[i]);
+                i--;
+            }
+        }
+
+        StartCoroutine(ExplodeGroup(objects));
     }
 }
