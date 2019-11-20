f8be935bba19637f41a01258ed0a398db8015fa4
Payton Musco <pjmusco.2017@mymail.becker.edu>
2019-11-19 23:31:38 -0500

    Added Groundwork for Riot shield

---
+++ Cell-Team-6-Game/Assets/RiotShield.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiotShield : MonoBehaviour
+{
+    public LayerMask layers;
+    private SimpleBullet playbull;
+    private GameObject RicochetBullet;
+    public GameObject playerBullet;
+    private Vector2 bulletTrans;
+    private Rigidbody2D rb;
+    private GameObject collidedBullet;
+    public float Countdown = 5f;
+
+    private void Start()
+    {
+        //Needed in order to get the power value of the bullet that we are using
+        playbull = playerBullet.GetComponent<SimpleBullet>();
+        StartCoroutine("ShieldTimer");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Finding the components of the bullet that collided
+        collidedBullet = collision.gameObject;
+        rb = collidedBullet.GetComponent<Rigidbody2D>();
+
+        if (collision.CompareTag("EnemyBullet"))
+        {
+            Debug.Log("should ricochet");
+            //Used adams racast ricocchet script to get the surface angle
+            RaycastHit2D surface = Physics2D.Raycast(collidedBullet.transform.position, rb.velocity.normalized,layers);
+
+            //Set a variable to the up direction of the bullet that collided
+            bulletTrans = collidedBullet.transform.up;
+
+            collidedBullet.SetActive(false);
+
+            //Sets an instanitation to instantiate a bullet in the inverse direction that the bullet came from
+            RicochetBullet = Instantiate(playerBullet, Vector2.Reflect(bulletTrans, surface.normal), Quaternion.identity);
+
+            //Instantiates a player bullet and adds force based on its power value
+            RicochetBullet.GetComponent<Rigidbody2D>().AddForce(transform.up * playbull.power);
+
+        }
+    }
+
+    IEnumerator ShieldTimer()
+    {
+        yield return new WaitForSeconds(Countdown);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Cell-Team-6-Game/Assets/Scripts/Abilities/RiotShield.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//All abilities must inherit from Ability and impliment a constructor with the ability name, and the two abstract classes below.
+public class RiotShieldAbility : Ability
+{
+    private GameObject RiotShield;
+    private GameObject RiotShieldUI;
+    public RiotShieldAbility()
+    {
+        abilityName = "RiotShield";
+        RiotShield = GameObject.Find("RiotShield");
+        RiotShieldUI = GameObject.Find("RiotshieldUI");
+    }
+
+    public override void OnPickup()
+    {
+        //All functionality to be activated imidately on pickup of this ability to be placed in here
+        Debug.Log("OnPickup called on ability: " + abilityName);
+        RiotShieldUI.SetActive(true);
+    }
+
+    protected override void CastAction()
+    {
+        //All actions to be taken upon a successful cast of this ability are to be placed here
+        Debug.Log("CastAction called on ability: " + abilityName);
+        RiotShield.SetActive(true);
+    }
+
+    protected override bool CastCondition()
+    {
+        //Any checks needed to be cast are placed here, return true if the conditions are met, false otherwise.
+        if (Input.GetKey(KeyCode.U)) { return false; }
+        else { return true; }
+    }
+}
