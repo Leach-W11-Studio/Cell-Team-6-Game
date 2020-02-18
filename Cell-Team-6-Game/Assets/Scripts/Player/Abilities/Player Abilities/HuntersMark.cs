f7333fad1bd447c1bc6a2e59dc3de1abd1423ccf
Ben Shackman <Bshackman@protonmail.com>
2020-02-18 13:35:49 -0500

    Hunters Mark Script and Condition

---
+++ Cell-Team-6-Game/Assets/Scripts/Player/Abilities/Player Abilities/HuntersMark.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuntersMark : Ability
+{
+    public HuntersMark()
+    {
+        abilityName = "HuntersMark";
+    }
+
+    private GameObject[] enemies;
+    private GameObject closestEnemy;
+
+    public float maximumMarkDistance = 20;
+
+    public override void OnPickup()
+    {
+        //Nothing
+    }
+
+    protected override void CastAction()
+    {
+
+    }
+
+    protected override bool CastCondition()
+    {
+        closestEnemy = null;
+
+        float currentMinDistance = maximumMarkDistance;
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies)
+        {
+            float curDistance = Vector2.Distance(enemy.transform.position, transform.position);
+            if (curDistance < currentMinDistance)
+            {
+                closestEnemy = enemy;
+                currentMinDistance = curDistance;
+            }
+        }
+
+        if (closestEnemy) { return true; }
+        else { return false; }
+    }
+}
